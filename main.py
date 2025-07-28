593b1bed02a2212ce1e0e6bcd007c733e10eb0e9
shu-kitamura <shusei3316@yahoo.co.jp>
2025-07-29 07:01:54 +0900

    add sidebar

--- main.py
+++ main.py
@@ -15,9 +15,13 @@ if __name__ == "__main__":
 
     st.title("Keijiban App")
 
+    st.sidebar.title("スレッド検索")
+    haystack = st.sidebar.text_input("スレッド名を入力", key="thread_input")
+    st.sidebar.subheader("スレッドの検索結果")
+    st.sidebar.markdown(f"- {haystack}")
+
     print_posts(st.session_state.posts)
 
-    # チャット入力フォーム
     msg = st.chat_input("チャットを開始", key="chat_input")
     if msg:
         st.session_state.posts.append({"text": msg, "time": st.session_state.get("time", datetime.now())})
