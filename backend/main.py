5de3f0c97e7177d3039375e19a528779410deb5f
shu-kitamura <shusei3316@yahoo.co.jp>
2025-09-27 14:16:53 +0900

    routing 変更

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,13 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
 from routers import threads, posts
 
 app = FastAPI()
 
-app.include_router(
-    threads.router,
-    prefix="/api/v1",
-)
-app.include_router(
-    posts.router,
-    prefix="/api/v1",
-)
+api_v1 = APIRouter(prefix="/api/v1")
+api_v1.include_router(threads.router)
+api_v1.include_router(posts.router)
+
+app.include_router(api_v1)
 
 @app.get("/")
 async def root():
