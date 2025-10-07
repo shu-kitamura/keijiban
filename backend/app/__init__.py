6bd599eee1e966f6a2e1ee262d123535a41654ea
shu-kitamura <shusei3316@yahoo.co.jp>
2025-10-07 21:55:58 +0900

    開発用DBを本番に合わせてSQL Server にした

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -9,6 +9,6 @@ def get_session():
     with Session(engine) as session:
         yield session
 
-pg_url = os.getenv("DATABASE_URL")
-engine = create_engine(pg_url, echo=True, future=True)
+database_url = os.getenv("DATABASE_URL")
+engine = create_engine(database_url, echo=True, future=True)
 sessionDep = Annotated[Session, Depends(get_session)]
